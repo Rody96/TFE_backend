8367976ef72370f1e5ad2328da92ec4acd6b5687
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2021-05-14 21:33:59 +0200

    adding port

--- backend/app.js
+++ backend/app.js
@@ -45,4 +45,9 @@ app.use("/users", usersRoute);
 
 ///////////////////// PORT //////////////////////////
 // set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+
+});
 /////////////////////////////////////////////////////
